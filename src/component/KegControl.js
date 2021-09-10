70923b31c219454d7f7f9844201e7de2bbc95ed5
Erika Debelis <erika.debelis@gmail.com>
2021-09-10 10:51:27 -0700

    add masterMenu to state and add callbacks

--- src/component/KegControl.js
+++ src/component/KegControl.js
@@ -4,10 +4,11 @@ import Button from 'react-bootstrap/Button';
 
 class KegControl extends React.Component {
 
-  constructor(prop) {
+  constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      masterMenu: []
     };
   }
 
@@ -15,21 +16,28 @@ class KegControl extends React.Component {
     this.setState(prevState => ({formVisibleOnPage: !prevState.formVisibleOnPage}));
   }
 
+  handleAddingNewKegToMenu = (newKeg) => {
+    const newMasterMenu = this.state.masterMenu.concat(newKeg);
+    this.setState({masterMenu: newMasterMenu, formVisibleOnPage:false });
+  }
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
     if(this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewKegForm />
+      currentlyVisibleState = <NewKegForm onNewKegCreation = {this.handleAddingNewKegToMenu} />
       buttonText = "Return to Menu";
     } else {
-      currentlyVisibleState = <Menu />
+      currentlyVisibleState = <Menu menu = {this.state.masterMenu} />
       buttonText = "Add Keg"
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
+        <button onClick = {this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
 }
+
+export default KegControl;
