c549e28fdf948a4d02d92b59bd032eb461377431
Erika Debelis <erika.debelis@gmail.com>
2021-09-10 10:32:17 -0700

    add beginning logic to kegcontrol and newkegform

--- src/component/KegControl.js
+++ src/component/KegControl.js
@@ -1,3 +1,27 @@
 import React from 'react';
 import Menu from './Menu';
 import Button from 'react-bootstrap/Button';
+
+class KegControl extends React.Component {
+
+  constructor(prop) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    if(this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm />
+    } else {
+      currentlyVisibleState = <Menu />
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+      </React.Fragment>
+    );
+  }
+}
