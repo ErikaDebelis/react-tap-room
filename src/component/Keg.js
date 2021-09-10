ab5c55a38ce8f42241f05b7aa4eb7f8a4d4bc781
Erika Debelis <erika.debelis@gmail.com>
2021-09-10 09:56:18 -0700

    add beginning logic to keg.js

--- src/component/Keg.js
+++ src/component/Keg.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+function Keg(props) {
+  return (
+    <React.Fragment>
+    <h4>{props.brand}</h4>
+    <h6>{props.name}</h6>
+    <h6>{props.alcoholContent}</h6>
+    <h6>{props.price}</h6>
+    </React.Fragment>
+  )
+}
+
+Keg.PropTypes = {
+  brand: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  alcoholContent: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired
+}
+
+export default Keg;
