3069e3a88862aaa27bbff93c625c3809bc0751be
Erika Debelis <erika.debelis@gmail.com>
2021-09-10 11:29:24 -0700

    add handlechangingselectedkeg in kegcontrol + kegdetail.js file

--- src/component/KegControl.js
+++ src/component/KegControl.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Menu from './Menu';
+import KegDetail from './KegDetail';
 import Button from 'react-bootstrap/Button';
 
 class KegControl extends React.Component {
@@ -8,7 +9,8 @@ class KegControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      masterMenu: []
+      masterMenu: [],
+      selectedKeg: null
     };
   }
 
@@ -18,17 +20,26 @@ class KegControl extends React.Component {
 
   handleAddingNewKegToMenu = (newKeg) => {
     const newMasterMenu = this.state.masterMenu.concat(newKeg);
-    this.setState({masterMenu: newMasterMenu, formVisibleOnPage:false });
+    this.setState({masterMenu: newMasterMenu, formVisibleOnPage: false });
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.masterMenu.filter(keg => keg.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
   }
 
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if(this.state.formVisibleOnPage) {
+
+    if(this.state.selectedKeg != null) {
+      currentlyVisibleState = <kegDetail keg = {this.state.selectedKeg} />
+    }
+    else if(this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm onNewKegCreation = {this.handleAddingNewKegToMenu} />
       buttonText = "Return to Menu";
     } else {
-      currentlyVisibleState = <Menu menu = {this.state.masterMenu} />
+      currentlyVisibleState = <Menu menu = {this.state.masterMenu} onKegSelection = {this.handleChangingSelectedKeg} />;
       buttonText = "Add Keg"
     }
     return (
---
+++ src/component/KegDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import Keg from './Keg';
+
+function KegDetail(props) {
+  const { keg } = props;
+
+  return(
+    <React.Fragment>
+      <h1>keg Detail</h1>
+      <h4>keg.brand</h4>
+      <h4>keg.name</h4>
+      <h4>keg.alcoholContent</h4>
+      <h4>keg.price</h4>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+KegDetail.propTypes = {
+  keg: PropTypes.object
+};
+
+export default KegDetail;
